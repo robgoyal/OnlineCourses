a966f830bcc0866315d10f0044066f13b35239a7
Robin Goyal <goyal.rob@gmail.com>
2017-05-10 12:59:52 -0400

    Working implementation of Percolation class

--- Algorithms/Percolation/Percolation.java
+++ Algorithms/Percolation/Percolation.java
@@ -1,9 +1,10 @@
 /* Name: Percolation.java
    Author: Robin Goyal
    Last Modified: May 10, 2017
-   Purpose: Determine if a NxN grid percolates (a path from top node
-            to bottom node)
+   Purpose: Open sites and determine if a NxN grid percolates (a path 
+            from top node to bottom node exists)
 */
+
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
@@ -26,12 +27,12 @@ public class Percolation {
         grid = new WeightedQuickUnionUF(n*n + 2);
         site = new boolean[n*n + 2];
 
-        // Set grid sites to false
+        // Set grid sites to blocked sites
         for (int i = 1; i <= gridSize; i++) {
             site[i] = false;
         }
 
-        // Set virtual top and virtual bottom sites to true
+        // Set virtual top and virtual bottom sites to open site
         site[0] = true;
         site[gridSize + 1] = true;
     }
@@ -47,26 +48,95 @@ public class Percolation {
         }
     }
 
+    // Convert 2D array access to 1D
+    private int xyTo1D(int i, int j) {
+        int gridIndex = (i * width) - (width - j);
+        return gridIndex;
+
+    }
+
     // Open site (row, col) if it is not opened already
     public void open(int row, int col) {
+        checkBounds(row, col);
+        int index = xyTo1D(row, col);
 
+        if (site[index]) {
+            return;
+        }
+
+        else {
+            // Figure out how to open site
+            openSite(row, col, index);
+            site[index] = true;
+        }
     }
 
-    // Is site (row, col) open?
+    // At most 4 union calls to open site
+    private void openSite(int row, int col, int index) {
+        
+        // join top index
+        if (row == 1) {
+            // connect to virtual top
+            grid.union(0, index);
+        }
+
+        else if (isOpen(row-1, col)) {
+            grid.union(index - width, index);
+        }
+
+        // join bottom index
+        if (row == width) {
+            // connect to virtual bottom
+            grid.union(gridSize + 1, index);
+        }
+
+        else if (isOpen(row+1, col)) {
+            grid.union(index + width, index);
+        }
+
+        // join left index
+        if (col != 1 && isOpen(row, col - 1)) {
+            grid.union(index - 1, index);
+        }
+
+        // join right index
+        if (col != width && isOpen(row, col + 1)) {
+            grid.union(index + 1, index);
+        }
+    }
+
+
+    // return if a site is open
     public boolean isOpen(int row, int col) {
+        checkBounds(row, col);
+        int index = xyTo1D(row, col);
 
+        return site[index];
     }
 
+    // return if a site is full
     public boolean isFull(int row, int col) {
+        checkBounds(row, col);
+        int index = xyTo1D(row, col);
 
+        // Check if site is connected to virtual top site
+        return grid.connected(0, index);
     }
 
+    // return the number of open sites
     public int numberOfOpenSites() {
+        int openSites = 0;
+        for (int i = 1; i < site.length; i++) {
+            if (site[i]) {
+                openSites++;
+            }
+        }
 
+        return openSites;
     }
 
-
+    // Check if virtual top and virtual bottom are connected
     public boolean percolates() {
-
+        return grid.connected(0, gridSize + 1);
     }
 }

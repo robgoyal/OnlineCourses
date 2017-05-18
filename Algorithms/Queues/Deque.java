7b721f43312e2091ebe7136481ebd73f5345acb7
Robin Goyal <goyal.rob@gmail.com>
2017-05-18 10:16:24 -0400

    Initial commit for Queues Assignment

---
+++ Algorithms/Queues/Deque.java
@@ -0,0 +1,52 @@
+/* Name: Deque.java
+   Author: Robin Goyal
+   Last-Modified: May 18, 2017
+   Purpose: Implement a double ended queue
+*/
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Deque<Item> implements Iterable<Item> {
+
+
+    // Construct an empty deque
+    public Deque() {
+
+    }
+
+    // Returns if the deque is empty
+    public boolean isEmpty() {
+
+    }
+
+    // Returns the number of items on the deque
+    public int size() {
+
+    }
+
+    // Adds an item to the front
+    public void addFirst(Item item) {
+
+    }
+
+    // Adds an item to the end
+    public void addLast(Item item) {
+
+    }
+
+    // Removes and returns the item at the front
+    public Item removeFirst() {
+
+    }
+
+    // Removes and returns the item at the end
+    public Item removeLast() {
+
+    }
+
+    // Returns an iterator over items in order from front to end
+}

43494879935bea83679ab1c2f87fda5fc6ac4275
Robin Goyal <goyal.rob@gmail.com>
2018-01-10 19:05:11 -0500

    Remove unnecessary comments

--- 6.00.1X/Final/longestRun.py
+++ 6.00.1X/Final/longestRun.py
@@ -1,7 +1,3 @@
-# Write a function called longestRun, which takes as a parameter a list of integers named L (assume L is not empty). This function returns the length of the longest run of monotonically increasing numbers occurring in L. A run of monotonically increasing numbers means that a number at position k+1 in the sequence is either greater than or equal to the number at position k in the sequence.
-
-# For example, if L = [10, 4, 6, 8, 3, 4, 5, 7, 7, 2] then your function should return the value 5 because the longest run of monotonically increasing integers in L is [3, 4, 5, 7, 7].
-
 # Name: longestRun.py
 # Author: Robin Goyal
 # Last-Modified: January 10, 2018

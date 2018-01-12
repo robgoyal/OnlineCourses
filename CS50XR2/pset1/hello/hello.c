4a1c57be9386ea164d5b2280394d9f97db49b052
robgoyal <goyal.rob@gmail.com>
2018-01-12 15:03:03 -0500

    Completed hello and mario of pset1

---
+++ CS50XR2/pset1/hello/hello.c
@@ -0,0 +1,12 @@
+/* Name: hello.c
+   Author: Robin Goyal
+   Last-Modified: January 12, 2018
+   Purpose: Print hello world to the screen
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+}

5b189e3af7114c2d2cd1de05b467f5f1cdf4fc25
robgoyal <goyal.rob@gmail.com>
2018-02-26 09:13:37 -0500

    Section 8 Exercises

---
+++ Bootcamp/Section8/AgeCalculator/script.js
@@ -0,0 +1,10 @@
+/* Name: script.js
+   Author: Robin Goyal
+   Last-Modified: February 26, 2018
+   Purpose: Calculate number of days given an age
+*/
+
+var age = parseInt(prompt("What is your age?"));
+var days = age * 365.25;
+
+alert(age + " years is roughly " + days + " days");

